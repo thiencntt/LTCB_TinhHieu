f72d28169e521b420d1f53029afbe5a508aa4560
ADMIN <ADMIN@LAB1-GIAOVIEN>
2024-02-26 08:21:32 +0700

    Add project files.

---
+++ LTCB_TinhHieu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, hieu;
+	cout << "CHUONG TRINH TINH HIEU 2 SO\n";
+	cout << "Nhap so thu nhat:";
+	cin >> so1;
+	cout << "Nhap so thu hai:";
+	cin >> so2;
+	hieu = so1 - so2;
+	cout << so1 << " - " << so2 << " = " << hieu << endl;
+	return 0;
+}
